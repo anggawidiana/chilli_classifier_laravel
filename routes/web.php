f93252a2cc132e446c1f5677f062237d5e4ee64b
kadekedwin <kadekedwin12@gmail.com>
2026-06-28 13:17:47 +0800

    add update and delete logic in histories list

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('/detection', 'Detection/Index')->name('detection.index');
     Route::get('/histories', [DetectionHistoryController::class, 'index'])->name('histories.index');
     Route::post('/histories', [DetectionHistoryController::class, 'store'])->name('histories.store');
+    Route::patch('/histories/{history}', [DetectionHistoryController::class, 'update'])->name('histories.update');
+    Route::delete('/histories/{history}', [DetectionHistoryController::class, 'destroy'])->name('histories.destroy');
 });
 
 require __DIR__.'/settings.php';
